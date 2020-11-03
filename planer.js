f5a5bfa7a4079d20a1fa408da35a0d04444c1cde
Jan Liebrecht <jan@liebrecht.at>
2020-11-03 16:30:33 +0100

    initial commit

---
+++ planer.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+var contactForm = document.getElementById("contact");
+
+
+
+var curr = document.getElementById(current_tab);
+
+if (curr === "contact"){
+    console.log("hi");
+}
+
+
+$('#regForm').forEach(val)
+
+
