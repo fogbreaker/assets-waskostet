df2d15e1a6e694d62b05f39e5219e64794e4d0e2
Jan Liebrecht <jan@liebrecht.at>
2020-11-30 17:19:18 +0100

    initial commit

--- planer.js
+++ planer.js
@@ -1,21 +1,33 @@
 
 
+let sucheHtml = '<div id="suche" style="display:none;"><p class="tab-title">Suche läuft...</p><fieldset class="pure-group"><img style="width: 800px; margin-left: -400px; left:50%;position: absolute;" alt="suche" src="https://assets.was-kostet.at/addresstool/images/suche.gif"></div>';
+
+$('#contact').before(sucheHtml);
 
 
 
+function doLastStep(){
+    $('#contact').css('display', 'none');
+    $('#suche').css('display','block');
+    $('#prevBtn').css('display','none');
+    $('#nextBtn').css('display','none');
+    $('#progress').css('display','none');
+    $('#submitBtn').css('display','none');
 
-
-var contactForm = document.getElementById("contact");
-
-
-
-var curr = document.getElementById(current_tab);
-
-if (curr === "contact"){
-    console.log("hi");
+    setTimeout(function() {
+        $('#suche').css('display','none');
+        $('#submitBtn').fadeIn();
+        $("#contact").fadeIn();
+        let tmp = $("#contact .tab-title").html();
+        $("#contact .tab-title").html("<span style='color: blue;'>2 Anbieter gefunden! </span>" +tmp);
+    }, 5000);
 }
+$('#Reparatur').bind('click', function(){
+    doLastStep();
+});
 
-
-$('#regForm').forEach(val)
+$('#wohnverhaeltnis').bind('click', function(){
+    doLastStep();
+});
 
 
