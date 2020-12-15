212451e825e3d62bde7f50488149cdbeb09e17fc
Jan Liebrecht <jan@liebrecht.at>
2020-12-15 17:18:45 +0100

    passt so

--- planer.js
+++ planer.js
@@ -1,12 +1,11 @@
 
 
-let sucheHtml = '<div id="suche" style="display:none;"><p class="tab-title">Suche lÃ¤uft...</p><fieldset class="pure-group"><img style="width: 800px; margin-left: -400px; left:50%;position: absolute;" alt="suche" src="https://assets.was-kostet.at/addresstool/images/suche.gif"></div>';
+let sucheHtml = '<div id="suche" style="display:none;"><p class="tab-title">Suche läuft...</p><fieldset class="pure-group"><img style="width: 800px; margin-left: -400px; left:50%;position: absolute;" alt="suche" src="https://assets.was-kostet.at/addresstool/images/suche.gif"></div>';
 
-$('#contact_finish').before(sucheHtml);
-
+$('#contact').before(sucheHtml);
 
 function doLastStep(){
-    $('#contact_finish').css('display', 'none');
+    $('#contact').css('display', 'none');
     $('#suche').css('display','block');
     $('#prevBtn').css('display','none');
     $('#nextBtn').css('display','none');
@@ -16,9 +15,23 @@ function doLastStep(){
     setTimeout(function() {
         $('#suche').css('display','none');
         $('#submitBtn').fadeIn();
-        $("#contact_finish").fadeIn();
-        let tmp = $("#contact_finish.tab-title").html();
-        $("#contact_finish.tab-title").html("<span style='color: blue;'>2 Anbieter gefunden! </span>" +tmp);
+        $("#contact").fadeIn();
+
+        $(".tab-title").html("<span style='color: blue;'>2 Anbieter gefunden! </span> Ihre Kontaktdaten");
     }, 5000);
 }
 
+document.getElementById("regForm").onkeypress = function (e) {
+    var key = e.charCode || e.keyCode || 0;
+    if (key === 13) {
+        e.preventDefault();
+        var ct = GetCurrentTab();
+        console.log("GetCurrentTab: " + ct);
+        if (ct !== "contact"){
+            $('#nextBtn').click();
+        }else{
+            $('#submitBtn').click();
+        }
+        //Next();
+    }
+};
