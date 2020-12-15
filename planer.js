e020cc2f197ceeb66ba21592b3bf0857a73265ed
Jan Liebrecht <jan@liebrecht.at>
2020-12-15 15:44:10 +0100

    Merge branch 'master' of C:\xampp\htdocs\assets-waskostet with conflicts.

--- planer.js
+++ planer.js
@@ -1,13 +1,12 @@
 
 
-let sucheHtml = '<div id="suche" style="display:none;"><p class="tab-title">Suche läuft...</p><fieldset class="pure-group"><img style="width: 800px; margin-left: -400px; left:50%;position: absolute;" alt="suche" src="https://assets.was-kostet.at/addresstool/images/suche.gif"></div>';
+let sucheHtml = '<div id="suche" style="display:none;"><p class="tab-title">Suche lÃ¤uft...</p><fieldset class="pure-group"><img style="width: 800px; margin-left: -400px; left:50%;position: absolute;" alt="suche" src="https://assets.was-kostet.at/addresstool/images/suche.gif"></div>';
 
-$('#contact').before(sucheHtml);
-
+$('#contact_finish').before(sucheHtml);
 
 
 function doLastStep(){
-    $('#contact').css('display', 'none');
+    $('#contact_finish').css('display', 'none');
     $('#suche').css('display','block');
     $('#prevBtn').css('display','none');
     $('#nextBtn').css('display','none');
@@ -17,17 +16,9 @@ function doLastStep(){
     setTimeout(function() {
         $('#suche').css('display','none');
         $('#submitBtn').fadeIn();
-        $("#contact").fadeIn();
-        let tmp = $("#contact .tab-title").html();
-        $("#contact .tab-title").html("<span style='color: blue;'>2 Anbieter gefunden! </span>" +tmp);
+        $("#contact_finish").fadeIn();
+        let tmp = $("#contact_finish.tab-title").html();
+        $("#contact_finish.tab-title").html("<span style='color: blue;'>2 Anbieter gefunden! </span>" +tmp);
     }, 5000);
 }
-$('#Reparatur').bind('click', function(){
-    doLastStep();
-});
-
-$('#wohnverhaeltnis').bind('click', function(){
-    doLastStep();
-});
-
 
