a95d14b6ea4f4cda6c88f9bc358319a303dcbc5f
Jan Liebrecht <jan@liebrecht.at>
2021-03-18 22:44:13 +0100

    implemented tracking

--- planer.js
+++ planer.js
@@ -4,6 +4,7 @@ let sucheHtml = '<div id="suche" style="display:none;"><p class="tab-title">Such
 
 $('#contact').before(sucheHtml);
 
+
 function doLastStep(){
     $('#contact').css('display', 'none');
     $('#suche').css('display','block');
@@ -11,6 +12,7 @@ function doLastStep(){
     $('#nextBtn').css('display','none');
     $('#progress').css('display','none');
     $('#submitBtn').css('display','none');
+    $('#regForm').css('min-height','500px');
 
     setTimeout(function() {
         $('#suche').css('display','none');
@@ -35,3 +37,83 @@ document.getElementById("regForm").onkeypress = function (e) {
         //Next();
     }
 };
+
+function getClickId() {
+    try {
+        var trackers = ga.getAll();
+        var i, len;
+        for (i = 0, len = trackers.length; i < len; i += 1) {
+            if (trackers[i].get('trackingId') === "UA-170828926-1") {
+                return trackers[i].get('clientId');
+            }
+        }
+    } catch(e) {}
+    return 'false';
+}
+
+$('#submitBtn').click(function(){
+
+    var filled = false;
+    pn_filled = !!$('#name').val();
+    ln_filled = !!$('#nachname').val();
+    ph_filled = !!$('#Tel').val();
+    em_filled = !!$('#email').val();
+    agb_filled = !!$('#agb').attr('checked');
+    message = {
+        "action":"Click",
+        "resource":"submit",
+        "label":"Angebote vergleichen",
+        "pn_filled": pn_filled,
+        "ln_filled": ln_filled,
+        "ph_filled": ph_filled,
+        "em_filled": em_filled,
+        "agb_filled": agb_filled,
+        "gcid": getClickId()
+    };
+    message = JSON.stringify(message);
+    $.ajax({
+        url: "https://assets.was-kostet.at/addresstool/report.php",
+        type: "POST",
+        data: "report=" + message,
+        context: document.body
+    }).done(function(response) {
+        console.log("GCLID" + response);
+    });
+});
+
+$(document).ready(function(){
+    window.onerror = function (msg, url, lineNo, columnNo, error) {
+        var string = msg.toLowerCase();
+        var substring = "script error";
+        if (string.indexOf(substring) > -1){
+            $.ajax({
+                url: "https://assets.was-kostet.at/addresstool/report.php",
+                type: "POST",
+                data: "error=Error kann nicht näher beschrieben werden. Das ist nicht gut." ,
+                context: document.body
+            }).done(function(response) {
+                console.log("Non-Classifiable Error was reported! Response from server was: " + response);
+            });
+        } else {
+            var message = [
+                'Message: ' + msg,
+                'URL: ' + url,
+                'Line: ' + lineNo,
+                'Column: ' + columnNo,
+                'Error object: ' + error
+            ];
+            message = JSON.stringify(message);
+
+            $.ajax({
+                url: "https://assets.was-kostet.at/addresstool/report.php",
+                type: "POST",
+                data: "error=" + message,
+                context: document.body
+            }).done(function(response) {
+                console.log("Error was reported! Response from server was: " + response);
+            });
+        }
+
+        return false;
+    };
+});
